be8b7b76010ac8e569761ac9f18b41e44cde7ebb
thieutk <104405554+thieutk@users.noreply.github.com>
2022-07-11 14:44:36 +0700

    change home

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import { WPPost } from '../libs/wpapi/interfaces'
 import { WPAPIURLFactory } from '../libs/wpapi/UrlBuilder'
 import { PostArchives } from '../components/archives/Posts';
 import { Divider, PageHeader } from 'antd'
-
+import styles from '../styles/Home.module.css'
 const urlBuilder = WPAPIURLFactory.init(
   process.env.WORDPRESS_URL,
 ).postType('posts').startAt(1).perPage(50).withEmbed()
@@ -23,9 +23,47 @@ export const Home:FC<{
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <PageHeader title="Recent Post" />
-      <Divider />
-      <PostArchives posts={posts} />
+       <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+
     </div>
   )
 }
